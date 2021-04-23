d9c75b1a9ce0073f8db337d8734eff142c05305d
Marcus Vinicius O. Freire <vinny.freire@gmail.com>
2021-04-23 13:00:10 -0300

    add author

--- index.js
+++ index.js
@@ -20,18 +20,44 @@ const delay = (timer = 300) => {
 
 let counter = 0;
 let tasks = []
+let authors = [
+  {
+    id: 1,
+    name: 'Marcus Freire'
+  },
+  {
+    id: 2,
+    name: 'Thiago Firmo'
+  },
+  {
+    id: 3,
+    name: 'Rafael Nunes'
+  }
+]
 
 const getIndexById = (id) => {
   return tasks.findIndex(x => x.id === parseInt(id, 10))
 }
 
 app.get('/tasks', async (req, res) => {
-  return res.json(tasks)
+  const result = tasks.map(task => {
+    const author = authors.find(a => a.id === task.author_id)
+    task.author = author;
+    return {
+      ...task,
+      author
+    }
+  })
+
+  return res.json(result)
 })
 
 app.get('/tasks/:id', async (req, res) => {
   const { id } = req.params
   const index = getIndexById(id)
+  const task = tasks[index]
+  const author = authors.find(a => a.id === task.author_id)
+  task.author = author;
   return res.json(tasks[index])
 })
 
@@ -49,17 +75,19 @@ app.patch('/tasks/:id', async (req, res) => {
 })
 
 app.post('/tasks', async (req, res) => {
-  const { title } = req.body
+  const { title, description, author_id } = req.body
 
   await delay();
-  if(!tasks.map(x=>x.title).includes(title)){
+  if (!tasks.map(x => x.title).includes(title)) {
     tasks.push({
       id: ++counter,
       title,
+      description,
+      author_id,
       completed: false
     });
     res.status(200).send()
-  }else{
+  } else {
     res.status(500).send('Não é possivel adicionar tarefas com o mesmo titulo.')
   }
 })
@@ -82,6 +110,10 @@ app.delete('/tasks-all', async (req, res) => {
 
 app.get('/', (req, res) => {
   res.send('Task API running')
+})
+
+app.get('/authors', (req, res) => {
+  res.json(authors)
 })
 
 app.listen(port, () => {
